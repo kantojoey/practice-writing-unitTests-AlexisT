a60263c2f8b881cae585f04e34ed393cd9cd4a8e
Alexis <atinoco999@gmail.com>
2025-09-09 22:14:41 -0500

    Added try/catch functionality to cart methods.

--- shopping-cart/cart.js
+++ shopping-cart/cart.js
@@ -1,16 +1,16 @@
 // 1. addItem(cart, item, quantity): Adds an item to the cart.
-function addItem(cart, item, quantity){
-    try{
-        if(Number(quantity) > 0){
-            for(let i = 1; i <= quantity; i++){
+function addItem(cart, item, quantity) {
+    try {
+        if (Number(quantity) > 0) {
+            for (let i = 1; i <= quantity; i++) {
                 cart.push(item);
             }
-        } else{
+        } else {
             throw new Error("Quantity must be greater than 0.");
         }
-    }catch(err){
+    } catch (err) {
         console.log("Error:", err.message);
-    }finally{
+    } finally {
         return cart;
     }
 };
@@ -20,19 +20,31 @@ function addItem(cart, item, quantity){
 // console.log(addItem(myCart, "bread", -1));
 
 // 2. removeItem(cart, item): Removes an item from the cart.
-function removeItem(cart, item){
+function removeItem(cart, item) {
     let index = cart.indexOf(item);
-    if(cart.includes(item)){
-        cart.splice(index, 1);
+    try {
+
+        if (cart.includes(item)) {
+            cart.splice(index, 1);
+        } else{
+            throw new Error("Item not found in cart.");
+        }
+    }catch(err){
+        console.log("Error:", err.message);
+    }finally{
+        return cart;
     }
-    return cart;
 };
+// let myCart2 = ["a", "b", "c"];
+// console.log(removeItem(myCart2, 'b'));
+// console.log(removeItem(myCart2, 'bananas'));
+// console.log(removeItem(myCart2,'c'));
 
 // 3. getTotalItems(cart): Returns the total number of items in the cart.
 // Tasks
-function getTotalItems(cart){
+function getTotalItems(cart) {
     return cart.length;
 };
 
 // ○ Export the methods using module.exports.
-module.exports = {addItem, removeItem, getTotalItems};
+module.exports = { addItem, removeItem, getTotalItems };
